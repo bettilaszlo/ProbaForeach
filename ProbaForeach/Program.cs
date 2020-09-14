29ee77b24b39a620f4a6c4cffe29d5d888d72bd7
bettilaszlo <laszlobetti3@gmail.com>
2020-09-14 13:50:52 +0200

    Add project files.

---
+++ ProbaForeach/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProbaForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tomb = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int szum = 0;
+
+            foreach (int t in tomb)
+            {
+                szum += t;
+                Console.WriteLine(t.ToString());
+            }
+            Console.WriteLine("Az összegük: {0}", szum);
+
+            Console.ReadKey();
+        }
+    }
+}
